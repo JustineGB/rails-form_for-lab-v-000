23c8ee842610e9dfc7168301474d4efba777f030
Justine B <justinegayton@gmail.com>
2018-12-21 17:40:22 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -13,7 +13,8 @@ class SchoolClassesController < ApplicationController
   end 
   
   def create
-   # @school_class = SchoolClass.create(params)
+     @school_class = SchoolClass.create(params.require(:school_class))
+        redirect_to school_class_path(@school_class)
   end 
   
   def edit 
