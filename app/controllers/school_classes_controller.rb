c6f48645118d639c34bdc3c748bb221ba062d4ec
Justine B <justinegayton@gmail.com>
2018-12-21 17:10:09 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,15 +1,12 @@
 class SchoolClassesController < ApplicationController
-  def index
-    @school_classes = SchoolClass.all
+  def new 
+    @school_classes = SchoolClass.new
   end 
   
   def show 
     @school_class = SchoolClass.find(params[:id])
   end 
-  
-  def new 
-    
-  end 
+
   
   def edit 
   end 
